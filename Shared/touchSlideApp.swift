d9382ff06dd59339a093fe032bc276f85f6c5756
Joakim Hellgren <hellojoakim@gmail.com>
2021-04-16 22:01:01 +0200

    Initial Commit

---
+++ Shared/touchSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchSlideApp.swift
+//  Shared
+//
+//  Created by Joakim Hellgren on 2021-04-16.
+//
+
+import SwiftUI
+
+@main
+struct touchSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
